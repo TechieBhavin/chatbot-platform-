f5fd479721349233aefde92d62010f9049061b2f
Patel Bhavin <techie.bhavin@gmail.com>
2026-01-16 17:08:01 +0530

    feat: environment setup and mongodb connection

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Connect DB
+connectDB();
 
 app.use(cors());
 app.use(express.json());
